006124dead5fde3eddb03db29d54e3cab844213d
Mikkel Luja Rasmussen <milr@itu.dk>
2020-06-11 18:04:20 +0200

    Init wte project

---
+++ README.md
@@ -0,0 +1,52 @@
+# What is wt-editor-cli?
+A command line editor for realtime updating of Windows Terminal settings
+
+![wt-editor-cli-showcase](https://user-images.githubusercontent.com/25123512/68077919-ba2a4980-fdcc-11e9-879f-6e1fecb6bb20.gif)
+
+|Info| |
+|-------|---------|
+| Build | ![release](https://github.com/BlackPhlox/wt-editor-cli/workflows/release/badge.svg) |
+| GitHub | [![GitHub version](https://badge.fury.io/gh/BlackPhlox%2Fwt-editor-cli.svg)](https://badge.fury.io/gh/BlackPhlox%2Fwt-editor-cli) |
+| NPM | [![npm version](https://badge.fury.io/js/wt-editor-cli.svg)](https://badge.fury.io/js/wt-editor-cli) |
+| Downloads |![npm](https://img.shields.io/npm/dt/wt-editor-cli?label=NPM%20Download)|
+| Twitter     | [![Twitter Follow](https://img.shields.io/twitter/follow/darkphlox?style=social)](https://twitter.com/darkphlox)      |
+
+## Related repositories
+  - [wt-editor-gui](https://github.com/BlackPhlox/wt-editor-gui) - A graphical user interface version of wt-editor-cli
+  - [Windows Terminal Editor (WTE)](https://github.com/BlackPhlox/wte) - A collection of both wt-editor-cli & wt-editor-gui
+  - [nateshmbhat's windows-terminal-tweaker](https://github.com/nateshmbhat/windows-terminal-tweaker) - A electron-based windows terminal editor
+
+# Prerequisites
+  - Running Windows 7 or later
+  - Have the latest version of Windows Terminal (Preview) installed
+  - Have Node.js 12 installed or later
+  - Have npm (Comes with Node.js)
+# Setup
+  - Clone this repo ```git clone https://github.com/BlackPhlox/wt-editor-cli.git``` or<br> using ```npm install -g wt-editor-cli ```
+  - (Important) If you already have configured your `settings.json`, I highly recommend you backup your file, just in case. The file can be found here: `%appdata%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\`
+  - Go into the downloaded folder `cd wt-editor-cli` if via git and `%appdata%\npm\node_modules\wt-editor-cli` if via npm
+  - Run `setup.bat`
+  - Run `wte`
+  
+  Additionally step
+  - If you are running using the [Linux-Subsystem](https://docs.microsoft.com/en-us/windows/wsl/install-win10) you can add `alias wte='cmd.exe /c wte'` in your `~/.bashrc` file.
+
+# Configuration
+When `setup.bat` has run, a _config.json_ file has been generated. There is currently only 3 properties you can change: assetPath(cli-explorer-selector's default path), questionColor and errorColor (Both is parsed using [chalk](https://www.npmjs.com/package/chalk)): 
+ ```json
+ { 
+    "jsonPath":"(Your appdata path)/../Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json", 
+    "assetPath":"(Your appdata path)/../Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/RoamingState", 
+    "questionColor":"lightgray", 
+    "errorColor":"red" 
+} 
+
+```
+
+# Contribution
+
+Any contribution is appreciated, their are no formalities, just create a pull request.
+
+### Creating Pull Requests
+  Push your commit to get it back up to your fork: git push origin HEAD
+  Visit Github, click handy “Pull request” button that it will make upon noticing your new branch.
---
+++ src/main.rs
@@ -0,0 +1,181 @@
+use dialoguer::{theme::ColorfulTheme, Select, Confirm};
+use linked_hash_map::LinkedHashMap;
+use std::path::Path;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, Write};
+use serde_json::{Value, from_value};
+use serde_json::json;
+use std::collections::HashMap;
+use serde_json::value::Value::Number;
+use std::any::Any;
+use std::fs;
+
+//Constants:
+static BACK: &str = "Back";
+static START_MENU: &str = "Main menu";
+static DEFAULT_PROMPT: &str = "What do you want to do?";
+
+macro_rules! gen_menu_path {
+    ($($menu: expr;$($access:expr),*);+) => {{
+        let mut map = ::linked_hash_map::LinkedHashMap::new();
+            $(
+                let mut v = vec![];
+                $(v.push(String::from($access));)*
+                map.insert(String::from($menu), v);
+            )*
+        map
+    }}
+}
+
+macro_rules! print_stack_ln {
+    ($a: expr) => {
+        $a.clone().iter_mut().for_each(| e | { print!("{} > ", e ) });
+        println!();
+    }
+}
+
+macro_rules! str_eq {
+    ($a: expr,$b: expr) => {
+        $a == $b
+    }
+}
+
+fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<HashMap<String, Value>, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let u = serde_json::from_reader(reader)?;
+    Ok(u)
+}
+
+fn set_json_value(hm: &mut HashMap<String, Value>, k: &str, v:Value) {
+    if hm.get(k).unwrap().type_id() == v.type_id() {
+        hm.insert(String::from(k), v);
+        //Callback - value changed
+    }
+}
+
+fn write(hm: &mut HashMap<String, Value>, filepath: &str){
+    let data = serde_json::to_string_pretty(&hm).unwrap();
+    let mut f = File::create(filepath).expect("Unable to create file");
+    f.write_all(data.as_bytes()).expect("Unable to write data");
+}
+
+//[Shepmaster 2015](https://stackoverflow.com/questions/32384594/how-to-check-whether-a-path-exists)
+pub fn path_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+fn save_prompt(hm: &mut HashMap<String, Value>, backup_path:String) {
+    if Confirm::new().with_prompt("Are you sure you want save current changes to backup? Previous backup will be overwritten").interact().unwrap() {
+        write(hm,backup_path.as_str());
+        println!("Current settings saved to backup");
+    } else {
+        println!("Save cancelled");
+    }
+}
+
+fn revert_prompt(hm: &mut HashMap<String, Value>, backup_path:String, settings_folder: String) -> HashMap<String, Value>{
+    if Confirm::new().with_prompt("Are you sure you want to load backup? Any current changes will be deleted").interact().unwrap() {
+        //Load backup
+        println!("Backup loaded");
+        let backup = read_user_from_file(backup_path).unwrap();
+        write(&mut backup.clone(),settings_folder.as_str());
+        backup
+    } else {
+        println!("Revert cancelled");
+        hm.clone()
+    }
+}
+
+
+fn main() {
+    //Load and set configs
+    let config = read_user_from_file("./src/config.json").unwrap();
+    let settings_folder : String = from_value(config.get("settings_folder_path").unwrap().clone()).unwrap();
+    let settings_path: String = (settings_folder +"settings.json");
+
+    //Load Schema
+    //let schema = read_user_from_file("./src/wt_schema.json").unwrap();
+
+    //Load wt settings
+    let mut hm = read_user_from_file(settings_path.clone()).unwrap();
+
+    //Do backup
+    let backup_path = settings_path.clone()+".backup";
+    if !path_exists(backup_path.clone().as_str()) {
+        write(&mut hm,backup_path.clone().as_str());
+    }
+
+    /*
+        let key = String::from("initialRows");
+        let val = hm.get(&key).unwrap();
+        set_json_value(&mut hm, &key, json!(2));
+        println!("{:#?}", hm.get(&key));
+        println!("{:#?}", config.keys());
+        write(&mut hm,settings_folder.clone().as_str());
+    */
+
+    //Setup menu
+    let mut l = gen_menu_path![
+        START_MENU;"Edit","Revert","Save","Exit";
+        "Edit";BACK,"Settings","Profiles","Schemes";
+        "Settings"; BACK;
+        "Profiles"; BACK;
+        "Schemes" ; BACK
+    ];
+
+    //Set start position
+    let mut current_menu_stack = vec![String::from(START_MENU)];
+
+    //Start menu-navigation loop
+    loop {
+        //println!();
+        //print_stack_ln!(current_menu_stack);
+        let a = prompt_menu(&mut current_menu_stack, &mut l);
+        match a {
+            b if str_eq!(b, "Program1") => { program1(); continue; },
+            b if str_eq!(b, "Save") => { save_prompt(&mut hm, backup_path.clone()); continue; },
+            b if str_eq!(b, "Revert") => { hm = revert_prompt(&mut hm, backup_path.clone(), settings_path.clone()); continue; },
+            b if str_eq!(b, BACK) => { current_menu_stack.pop(); continue; },
+            b if str_eq!(b, "Exit") => { break; },
+            _ => { current_menu_stack.push(a); continue; }
+        };
+    }
+}
+
+fn prompt_menu(menu_stack: &mut Vec<String>, menu_relations: &mut LinkedHashMap<String,Vec<String>>) -> String {
+    let prompt_combo = menu_relations;
+    let current = menu_stack.last().unwrap();//Latest pushed
+    let m = match prompt_combo.get( current) {
+        Some(t) => t,
+        None if current == &String::from(BACK) => prompt_combo.get(menu_stack.first().unwrap()).unwrap(), //Back selected, get previous stack and pop current
+        _ => {
+            //Not found, pop from stack and go back to previous/last item in the stack
+            eprintln!("Error : Item not found");
+            menu_stack.pop();
+            prompt_combo.get(menu_stack.last().unwrap()).unwrap()
+        }
+    };
+    let selections: Vec<String> = m.clone();
+    let r = setup_prompt(menu_stack.last().unwrap(),(menu_stack.join(" > ")).to_string(), &selections);
+
+    let sel = &selections.get(r);
+    sel.unwrap().clone()
+}
+
+fn setup_prompt(_: &str, prompt: String, s: &[String]) -> usize {
+    Select::with_theme(&ColorfulTheme::default())
+        .with_prompt(prompt)
+        .with_no_prompt_confirmation()
+        .default(0)
+        .items(&s)
+        .interact()
+        .unwrap()
+}
+
+fn program1() {
+    println!("Program1 has completed");
+}
+
