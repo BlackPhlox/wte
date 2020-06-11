a2c4d47f03304a50e232fc6e9fae5d56721ae2a4
Mikkel Luja Rasmussen <milr@itu.dk>
2020-06-11 19:34:41 +0200

    Added .bat file creation

--- src/setup.bat
+++ src/setup.bat
@@ -37,9 +37,9 @@ echo.
 ::echo SUCCESS: Libraries was installed.
 ::echo.
 
-::echo 4. Create batfile
-::echo.
-::echo ^@echo off ^& cmd /C "cd %cd% & node index.js" > "wte.bat"
+echo 4. Create batfile
+echo.
+echo ^@echo off ^& cmd /C "cd %cd%\..\ & cargo run" > "wte.bat"
 
 echo Complete! Restart your terminal and then type "wte" to run the application
 pause
